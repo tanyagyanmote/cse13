3d257b4f01fa17b9635a37b77757cb71c5766d1c
tanya <tgyanmot@ucsc.edu>
2022-10-10 20:36:47 -0700

    VIRTUAL: added element to array

--- asgn3/dreidel.c
+++ asgn3/dreidel.c
@@ -1,7 +1,7 @@
  
 
 char * nato_abc [] = { "Alfa",
-"Baker", "Charlie", "Delta", "Echo", "Foxtrot","Rishitass"
+"Baker", "Charlie", "Delta", "Echo", "Foxtrot","Rishitass","Tanya"
 }
 
 char spin_dreidel(void){}
