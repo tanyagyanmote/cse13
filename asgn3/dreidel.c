4cf36cd060fbe677ddb20acc0d23008ca1426bc1
Tanya Gyanmote <tgyanmot@ucsc.edu>
2022-10-13 23:17:30 -0700

    updating dreidel.c 10/13

--- asgn3/dreidel.c
+++ asgn3/dreidel.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "mtrand.h"
+#include <stdbool.h>
 
 char spin_dreidel(void){
     
@@ -7,7 +8,9 @@ char spin_dreidel(void){
     char h = 'H';
     char n = 'N';
     char s = 'S';
+
     uint64_t rand_num = mtrand_rand64()%4;
+
     if (rand_num==0){
         return g;
     }
@@ -27,63 +30,61 @@ char spin_dreidel(void){
  }
 
 int play_game (int n_players , int coins_per_player, int * n_rounds ){
+
     int players [n_players];
     int remaining = n_players;
     int pot = 0;
+    int num = 0;
 
-    for (int i = 0; i <= n_players; i++){
+    for (int i = 0; i < n_players; i++){
         players[i] = coins_per_player;
     }
 
-    //int i = 0;
-
-    while(i < 5){
+    while(true){
+        char turn = spin_dreidel();
         for(int j = 0; j < n_players; j++){
-            char turn = spin_dreidel();
             if (turn=='G'){
                 players[j] += pot;
                 pot = 0;
             }
-            if (turn=='H'){
+
+            if (turn=='H' && pot != 0){
                 players[j] += pot/2;
                 pot -= pot/2;
             }
-            if (turn=='N'){
-                break;
-            }
+ 
             if (turn=='S'){
-                if(players[j]==0){
-                    continue;
+                if(players[j] == 0){
+                    remaining -=1;
                 }
-                players[j] -= 1;
-                pot += 1;
-            }
-
-            //printf("%i", turn);
-            //printf("%i\n", players[j]);
-            //printf("%i\n", * n_rounds);
-            
-            for (int k =0; k < n_players;k+=1){
-                if(players[k]) < 0){
-                    remaining -=1
+                else if(players[j] > 0){
+                    players[j] -= 1;
+                    pot += 1;
                 }
             }
 
-            if (remanining == 1){
-                for (int l = 0; l<n_players; l+=1;){
-                    if (players[l] >= 0){
-                        return l;
+            printf("player:     %i\n", j);
+            printf("coins:      %i\n", players[j]);
+            printf("round:      %i\n", num);
+            //printf("remaining:  %i\n", remaining);
+            //printf("turn:       %c\n", turn);
+
+            if (remaining == 1){
+                int max = 0;
+                int p = 0;
+                for (int l = 0; l < n_players; l+=1){
+                    if(players[l] > max){
+                        max = players[l];
+                        p = l;
                     }
                 }
-            }
-
-            * n_rounds += 1;
-            i += 1;
+                return p;
+            } 
 
         }
+        num += 1;
+        * n_rounds += 1;
+
     }
-
-    return -1;
-
 }
 
