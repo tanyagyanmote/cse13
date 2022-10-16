3185620eecdbb8e9163fe3734a9930b2253d4ceb
Tanya Gyanmote <tgyanmot@ucsc.edu>
2022-10-16 13:54:43 -0700

    updating dreidel.c

--- asgn3/dreidel.c
+++ asgn3/dreidel.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "mtrand.h"
 #include <stdbool.h>
+#include "dreidel.h"
+
+
 
 char spin_dreidel(void){
     
@@ -34,57 +37,56 @@ int play_game (int n_players , int coins_per_player, int * n_rounds ){
     int players [n_players];
     int remaining = n_players;
     int pot = 0;
-    int num = 0;
 
     for (int i = 0; i < n_players; i++){
         players[i] = coins_per_player;
     }
 
     while(true){
-        char turn = spin_dreidel();
         for(int j = 0; j < n_players; j++){
-            if (turn=='G'){
-                players[j] += pot;
-                pot = 0;
-            }
+            if(players[j] != -1){
+                char turn = spin_dreidel();
 
-            if (turn=='H' && pot != 0){
-                players[j] += pot/2;
-                pot -= pot/2;
-            }
- 
-            if (turn=='S'){
-                if(players[j] == 0){
-                    remaining -=1;
+                if (turn=='G'){
+                    players[j] += pot;
+                    pot = 0;
                 }
-                else if(players[j] > 0){
-                    players[j] -= 1;
-                    pot += 1;
+
+                if (turn=='H'){
+                    players[j] += pot/2;
+                    pot -= pot/2;
                 }
-            }
-
-            printf("player:     %i\n", j);
-            printf("coins:      %i\n", players[j]);
-            printf("round:      %i\n", num);
-            //printf("remaining:  %i\n", remaining);
-            //printf("turn:       %c\n", turn);
-
-            if (remaining == 1){
-                int max = 0;
-                int p = 0;
-                for (int l = 0; l < n_players; l+=1){
-                    if(players[l] > max){
-                        max = players[l];
-                        p = l;
+    
+                if (turn=='S'){
+                    if(players[j] == 0){
+                        remaining -=1;
+                        players[j] = -1;
+                        if(test_v==true){
+                            printf("%s: eliminated in round %d of a %d player game. \n", list_players[j], *n_rounds, n_players);
+                        }
+                    }
+                    else{
+                        players[j] -= 1;
+                        pot += 1;
                     }
                 }
-                return p;
-            } 
 
+                if (remaining == 1){
+                    * n_rounds += 1;
+                    for (int l = 0; l < n_players; l+=1){
+                        if(players[l] != -1){
+                            return l;
+                        }
+                    }
+                } 
+            }
         }
-        num += 1;
+
         * n_rounds += 1;
 
+
     }
+    return 0;
 }
+
 
