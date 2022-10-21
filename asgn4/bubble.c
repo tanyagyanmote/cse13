bb179a05af0c976a6eeb68726ce4d00604b49069
Tanya Gyanmote <tgyanmot@ucsc.edu>
2022-10-21 00:44:39 -0700

    updating bubble sort

--- asgn4/bubble.c
+++ asgn4/bubble.c
@@ -11,20 +11,22 @@
 
 
 void bubble_sort(Stats *stats, uint32_t *arr, uint32_t n_elements){
-    bool swapped = False;
-    for(int i = 0; i<(n_elements-1); i++){
-        swapped = True;
-        for(int j = (n_elements-1); j < -1; j+=i){
-            if (arr[j] < arr[j-1]){
-                swap(stats, arr[j],arr[i]);
-                swapped = True;
+   
+    for(uint32_t i = 0; i < (n_elements-1); i++){
+        bool swapped = false;
+        for(uint32_t j = (n_elements-1); j > i; j-=1){
+            if (cmp(stats,arr[j],arr[j-1]) == -1){
+                swap(stats, &arr[j],&arr[j-1]);
+                swapped = true;
             }
         }
-        if(swapped == False){
+        if(!swapped){
             break;
         }
     }
 
 }
+
+
 
 
