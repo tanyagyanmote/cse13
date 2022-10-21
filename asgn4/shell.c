a27a9dead8f6a6e0de2a9ec6c11a7368a3235223
Tanya Gyanmote <tgyanmot@ucsc.edu>
2022-10-21 00:45:51 -0700

    updated shell with stats, and piazza implementation

--- asgn4/shell.c
+++ asgn4/shell.c
@@ -8,25 +8,31 @@
 #include <stdio.h>
 #include <unistd.h>
 
+uint32_t gap(uint32_t n){
+    if (n<=2){
+        return n-1;
+    }
+    if(n<=0){
+        return 0;
+    }
+    else{
+        return (n * 5) / 11;
+    }
+}
+
 void shell_sort(Stats *stats, uint32_t *arr, uint32_t n_elements){
-    uint32_t temp;
-    uint32_t gap;
-    for(uint32_t n = n_elements, n >= 1; n-=gap){
-        if (n<=2){
-            n=1;
-        }
-        else{
-            n = n * 5 / 11;
-        }
-        gap = n;
-        for (uint32_t i = gap; i < n; i++) {
-            for(j=i; j < gap-1; j-=gap){
-                if (arr[j] < arr[j-gap]){
-                    temp = arr[j];
-                    arr[j] = arr[j-gap];
-                    arr[j-gap] = arr[j];
-                }
+    for (uint32_t step = gap(n_elements); step > 0; step = gap(step)){
+        for (uint32_t i = step; i < n_elements; i++) {
+            uint32_t j = i;
+            uint32_t temp = move(stats, arr[i]);
+            while (j >= step && cmp(stats, temp, arr[j - step]) == -1) {
+                arr[j] = move(stats, arr[j - step]);
+                move(stats,arr[j]);
+                //move(stats,arr[j-step]);
+                j = j - step;
             }
+            arr[j] = move(stats, temp);
+            move(stats,arr[j]);
         }
     }
 }
