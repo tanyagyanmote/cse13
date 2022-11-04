99ff922b674a71b37ccd7623897abcff1c3f95da
Tanya Gyanmote <tgyanmot@ucsc.edu>
2022-11-03 23:27:17 -0700

    pushing keygen with updates to help

--- asgn5/keygen.c
+++ asgn5/keygen.c
@@ -10,17 +10,17 @@
 #define OPTIONS "b:i:n:d:s:vh"
 
 void print_h(){
-    printf("Usage: ./keygen-dist [options]\n");
-    printf("  ./keygen-dist generates a public / private key pair, placing the keys into the public and private\n");
-    printf("  key files as specified below. The keys have a modulus (n) whose length is specified in\n");
-    printf("  the program options.\n");
-    printf("    -s <seed>   : Use <seed> as the random number seed. Default: time()\n");
-    printf("    -b <bits>   : Public modulus n must have at least <bits> bits. Default: 1024\n");
-    printf("    -i <iters>  : Run <iters> Miller-Rabin iterations for primality testing. Default: 50\n");
-    printf("    -n <pbfile> : Public key file is <pbfile>. Default: rsa.pub\n");
-    printf("    -d <pvfile> : Private key file is <pvfile>. Default: rsa.priv\n");
-    printf("    -v          : Enable verbose output.\n");
-    printf("    -h          : Display program synopsis and usage.\n");
+    fprintf(stderr,"Usage: ./keygen-dist [options]\n");
+    fprintf(stderr,"  ./keygen-dist generates a public / private key pair, placing the keys into the public and private\n");
+    fprintf(stderr,"  key files as specified below. The keys have a modulus (n) whose length is specified in\n");
+    fprintf(stderr,"  the program options.\n");
+    fprintf(stderr,"    -s <seed>   : Use <seed> as the random number seed. Default: time()\n");
+    fprintf(stderr,"    -b <bits>   : Public modulus n must have at least <bits> bits. Default: 1024\n");
+    fprintf(stderr,"    -i <iters>  : Run <iters> Miller-Rabin iterations for primality testing. Default: 50\n");
+    fprintf(stderr,"    -n <pbfile> : Public key file is <pbfile>. Default: rsa.pub\n");
+    fprintf(stderr,"    -d <pvfile> : Private key file is <pvfile>. Default: rsa.priv\n");
+    fprintf(stderr,"    -v          : Enable verbose output.\n");
+    fprintf(stderr,"    -h          : Display program synopsis and usage.\n");
 
 }
 
@@ -38,15 +38,23 @@ int main(int argc, char **argv) {
         switch (opt) {
         case 'b': 
             bit_count = strtoul(optarg, NULL, 10);
+            if (bit_count > 4096 || bit_count < 50){
+                print_h();
+                return -1;
+            }
             break;
         case 'i': 
             iters = strtoul(optarg, NULL, 10);
+            if (iters > 500 || iters < 1){
+                print_h();
+                return -1;
+            }
             break;
         case 'n':
             pub_file = fopen(optarg, "w");
             if(pub_file == NULL){
                 printf("File empty - invalid");
-                return 0;
+                return -1;
             }
             pb = true;
             break;
@@ -54,22 +62,26 @@ int main(int argc, char **argv) {
             priv_file = fopen(optarg,"w");
             if(priv_file == NULL){
                 printf("File empty - invalid");
-                return 0;
+                return -1;
             }
             pv = true;
             break;
         case 's':
             seed = strtoul(optarg, NULL, 10);
+            if (seed < 0){
+                print_h();
+                return -1;
+            }
             break;
         case 'h':
             print_h();
-            break;
+            return 0;
         case 'v':
             verbose = true;
             break;
         default:
             print_h();
-            return 0;
+            return -1;
         }
     }
 
@@ -77,7 +89,7 @@ int main(int argc, char **argv) {
         priv_file = fopen("rsa.priv", "w");
         if(priv_file == NULL){
             printf("File empty - invalid");
-            return 0;
+            return -1;
         }
     }
 
@@ -85,7 +97,7 @@ int main(int argc, char **argv) {
         pub_file = fopen("rsa.pub", "w");
         if(pub_file == NULL){
             printf("File empty - invalid");
-            return 0;
+            return -1;
         }
     }
 
@@ -98,24 +110,29 @@ int main(int argc, char **argv) {
     char *user;
     user = getenv("USER");
 
-    mpz_set_str(name, user, 62);
-
     rsa_make_pub(p, q, n, e, bit_count, iters);
     rsa_make_priv(d, e, p, q);
 
+    mpz_set_str(name, user, 62);
+
+    rsa_sign(s, name, d, n);
+    
     rsa_write_pub(n, e, s, user, pub_file);
     rsa_write_priv(n, d, priv_file);
 
-    rsa_sign(s, name, d, n);
+    // gmp_printf("m: %Zd\n", name);
+    // gmp_printf("exponent: %Zd\n", e);
+    // gmp_printf("n: %Zd\n", n);
+    // gmp_printf("s: %Zd\n", s);
 
     if (verbose == true) {
-        printf("username = %s\n", user);
-        gmp_printf("user signature (%d bits) = %Zd\n", mpz_sizeinbase(s, 2), s);
-        gmp_printf("p (%d bits) = %Zd\n", mpz_sizeinbase(p, 2), p);
-        gmp_printf("q (%d bits) = %Zd\n", mpz_sizeinbase(q, 2), q);
-        gmp_printf("n - modulus (%d bits) = %Zd\n", mpz_sizeinbase(n, 2), n);
-        gmp_printf("e - public exponent (%d bits) = %Zd\n", mpz_sizeinbase(e, 2), e);
-        gmp_printf("d - private exponent (%d bits) = %Zd\n", mpz_sizeinbase(d, 2), d);
+        fprintf(stderr,"username = %s\n", user);
+        gmp_fprintf(stderr,"user signature (%d bits) = %Zd\n", mpz_sizeinbase(s, 2), s);
+        gmp_fprintf(stderr,"p (%d bits) = %Zd\n", mpz_sizeinbase(p, 2), p);
+        gmp_fprintf(stderr,"q (%d bits) = %Zd\n", mpz_sizeinbase(q, 2), q);
+        gmp_fprintf(stderr,"n - modulus (%d bits) = %Zd\n", mpz_sizeinbase(n, 2), n);
+        gmp_fprintf(stderr,"e - public exponent (%d bits) = %Zd\n", mpz_sizeinbase(e, 2), e);
+        gmp_fprintf(stderr,"d - private exponent (%d bits) = %Zd\n", mpz_sizeinbase(d, 2), d);
     }
 
     randstate_clear();
