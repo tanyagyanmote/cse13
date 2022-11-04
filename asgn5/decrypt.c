f9456bc01478191394a49a13d92cb834037f1887
Tanya Gyanmote <tgyanmot@ucsc.edu>
2022-11-04 12:59:48 -0700

    editing decrypt.c

--- asgn5/decrypt.c
+++ asgn5/decrypt.c
@@ -14,14 +14,14 @@
 #define OPTIONS "i:o:n:vh"
 
 void print_h(){
-    printf("Usage: ./decrypt-dist [options]\n");
-    printf("  ./decrypt-dist decrypts an input file using the specified private key file,\n");
-    printf("  writing the result to the specified output file.\n");
-    printf("    -i <infile> : Read input from <infile>. Default: standard input.\n");
-    printf("    -o <outfile>: Write output to <outfile>. Default: standard output.\n");
-    printf("    -n <keyfile>: Private key is in <keyfile>. Default: rsa.priv.\n");
-    printf("    -v          : Enable verbose output.\n");
-    printf("    -h          : Display program synopsis and usage.\n");
+    fprintf(stderr,"Usage: ./decrypt [options]\n");
+    fprintf(stderr,"  ./decrypt decrypts an input file using the specified private key file,\n");
+    fprintf(stderr,"  writing the result to the specified output file.\n");
+    fprintf(stderr,"    -i <infile> : Read input from <infile>. Default: standard input.\n");
+    fprintf(stderr,"    -o <outfile>: Write output to <outfile>. Default: standard output.\n");
+    fprintf(stderr,"    -n <keyfile>: Private key is in <keyfile>. Default: rsa.priv.\n");
+    fprintf(stderr,"    -v          : Enable verbose output.\n");
+    fprintf(stderr,"    -h          : Display program synopsis and usage.\n");
 }
 
 
@@ -30,49 +30,53 @@ int main(int argc, char **argv) {
     bool verbose = false;
     FILE *inputfile = stdin;
     FILE *outputfile = stdout;
-    FILE *privfile;
+    FILE *privfile =NULL;
+    char priv_s[] = "rsa.priv";
     while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
         switch (opt) {
             case 'i':
                 inputfile = fopen(optarg, "r");
                 if(inputfile == NULL){
                     printf("File empty - invalid");
-                    return 0;
+                    return -1;
                 }
                 break;
             case 'o': 
                 outputfile = fopen(optarg, "w");
                 if(outputfile == NULL){
-                    printf("File empty - invalid");
-                    return 0;
+                    fprintf(stderr,"File empty - invalid");
+                    return -1;
                 }
                 break;
             case 'n':
-                privfile = fopen(optarg, "r");
-                if (privfile == NULL) {
-                    printf("File empty - invalid");
-                }
+                strcpy(priv_s,optarg);
                 break;
             case 'v':
                 verbose = true;
                 break; 
             case 'h':
                 print_h();
-                break;
+                return 0;
             default:
                 print_h();
-                return 0; 
+                return -1; 
+        }
+
+        privfile = fopen(priv_s, "r");
+        if (privfile == NULL) {
+            fprintf(stderr,"File empty - invalid");
+                return -1;
         }
 
         mpz_t n, d;
         mpz_inits(n, d, NULL);
 
-        if (verbose) {
-            gmp_printf("n (%d bits) = %Zd\n", mpz_sizeinbase(n, 2), n);
-            gmp_printf("d (%d bits) = %Zd\n", mpz_sizeinbase(d, 2), d);
-        }
-
         rsa_read_priv(n, d, privfile);
+
+        if (verbose) {
+            gmp_fprintf(stderr,"n (%d bits) = %Zd\n", mpz_sizeinbase(n, 2), n);
+            gmp_fprintf(stderr,"d (%d bits) = %Zd\n", mpz_sizeinbase(d, 2), d);
+        }
 
         rsa_decrypt_file(inputfile, outputfile, n, d);
 
