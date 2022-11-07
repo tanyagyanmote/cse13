697afe06b44e51dda5db2a523bc49d6c55447e3d
Tanya Gyanmote <tgyanmot@ucsc.edu>
2022-11-06 17:19:13 -0800

    with format randstate.c

--- asgn5/randstate.c
+++ asgn5/randstate.c
@@ -1,17 +1,14 @@
+#include "randstate.h"
+#include <gmp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <gmp.h>
-#include "randstate.h"
 
 gmp_randstate_t state;
 
 void randstate_init(uint64_t seed) {
-    srandom(seed);
-    gmp_randinit_mt(state);
-    gmp_randseed_ui(state, seed);
+  srandom(seed);
+  gmp_randinit_mt(state);
+  gmp_randseed_ui(state, seed);
 }
 
-void randstate_clear(void) {
-    gmp_randclear(state);
-}
-
+void randstate_clear(void) { gmp_randclear(state); }
