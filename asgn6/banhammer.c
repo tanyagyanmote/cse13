3134fa3ba1741af60bc65c038396c1670364e3f0
Tanya Gyanmote <tgyanmot@ucsc.edu>
2022-11-20 02:33:00 -0800

    editing banhammer

--- asgn6/banhammer.c
+++ asgn6/banhammer.c
@@ -32,7 +32,6 @@ int main(int argc, char **argv) {
     uint32_t table_size = 10000;
     uint32_t f_size = 1<<19;
     bool mtf = false;
-    bool word = false;
     while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
         switch (opt) {
             case 's':
@@ -88,7 +87,7 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    while((word = next_word(p_bad,badspeak)) != false){
+    while((next_word(p_bad,badspeak)) != false){
         //printf("%s\n", badspeak);
         bf_insert(bf,badspeak);
         ht_insert(ht,badspeak,NULL);
@@ -104,8 +103,8 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    while((word = next_word(p_new,oldspeak)) != false){
-        next_word(p_new,newspeak);
+    while((next_word(p_new,oldspeak)) && next_word(p_new,newspeak)){
+        //next_word(p_new,newspeak);
         bf_insert(bf,oldspeak);
         ht_insert(ht,oldspeak,newspeak);
     }
